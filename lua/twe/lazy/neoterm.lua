258c7b2b7ab6f571570fad7ce8f1080b34c463ab
indisposed <dev@indisposition.net>
2024-02-17 19:29:09 +1300

    tidy
    tidied up unused files/plugins
    TODO: Term needs work - keybindings should be done nicely with descriptions

--- lua/twe/lazy/neoterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- "nyngwang/NeoTerm.lua",
-  -- config = function()
-  --   require('neo-term').setup({})
-  --   vim.keymap.set('n', '<leader><Tab>', function() vim.cmd('NeoTermToggle') end)
-  --   vim.keymap.set('t', '<leader>', " ")
-  --   vim.keymap.set('t', '<Esc>', function() vim.cmd('NeoTermEnterNormal') end)
-  -- end
-}
